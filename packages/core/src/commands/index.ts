596dabec92884a40aece2084e98c564077351d73
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2025-12-04 15:25:48 +0300

    feat(RF-1): add aim command

--- packages/core/src/commands/index.ts
+++ packages/core/src/commands/index.ts
@@ -6,6 +6,7 @@
  */
 
 export { Command, type CommandContext } from './base';
+export { AimCommand, type AimInput, type AimOutput } from './aim-command';
 export { FireCommand, type FireInput, type FireOutput } from './fire-command';
 export { TargetCommand, type TargetInput, type TargetOutput } from './target-command';
 export { ReloadCommand, type ReloadInput, type ReloadOutput } from './reload-command';
