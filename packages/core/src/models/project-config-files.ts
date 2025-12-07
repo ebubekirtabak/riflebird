b9c458486c92e475a2499708f0168f71ca01160f
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2025-12-07 14:01:18 +0300

    fix(RF-1): import missing models

---
+++ packages/core/src/models/project-config-files.ts
@@ -0,0 +1,31 @@
+export type ConfigFile = {
+  type: string;
+  name?: string;
+  fileLang?: string;
+  version?: string;
+  configFile: string;
+  configFilePath: string
+};
+
+export type ProjectConfigFiles = {
+  framework: ConfigFile;
+  language: string;
+  packageManager: string;
+  libs: {
+    core: string[],
+    testing: string[],
+    styling: string[],
+  },
+  testFrameworks: TestFrameworksConfig,
+  linting: ConfigFile;
+  formatting: ConfigFile;
+  languageConfig: ConfigFile;
+  importantConfigFiles: Record<string, ConfigFile>;
+};
+
+export type TestFrameworksConfig = {
+  unit?: ConfigFile;
+  e2e?: ConfigFile;
+  visual?: ConfigFile;
+  performance?: ConfigFile;
+};
