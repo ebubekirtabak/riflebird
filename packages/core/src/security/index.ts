3308fed5a9c21a3e67a4a0f78e25a0f6ecc35e18
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2025-12-08 13:06:20 +0300

    chore(RF-1): improve re-exporting

--- packages/core/src/security/index.ts
+++ packages/core/src/security/index.ts
@@ -1,12 +1,10 @@
+export { SecretScanner, type DetectedSecret, type SanitizationResult, type ScanOptions } from './secret-scanner';
+export { SanitizationLogger, sanitizationLogger, type SanitizationEvent } from './sanitization-logger';
+export { SECRET_PATTERNS, FALSE_POSITIVE_PATTERNS, SCANNABLE_EXTENSIONS, EXCLUDE_PATTERNS, type SecretType, type SecretPattern } from './secret-patterns';
+
 import { SecretScanner } from './secret-scanner';
 import { sanitizationLogger } from './sanitization-logger';
-export { SecretScanner, type DetectedSecret, type SanitizationResult, type ScanOptions } from './secret-scanner';
-export {
-  SanitizationLogger,
-  sanitizationLogger,
-  type SanitizationEvent,
-} from './sanitization-logger';
-export { SECRET_PATTERNS, FALSE_POSITIVE_PATTERNS, SCANNABLE_EXTENSIONS, EXCLUDE_PATTERNS, type SecretType, type SecretPattern } from './secret-patterns';
+
 
 /**
  * Convenience function to sanitize code with logging
