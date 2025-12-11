b1df99a778716f52eac77c0bea9740d6c009dc77
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2025-12-12 00:37:10 +0300

    fix(RF-4): remove duplicated file stats functions

--- packages/core/src/utils/file-finder.ts
+++ packages/core/src/utils/file-finder.ts
@@ -137,27 +137,3 @@ function flattenAndFilterFiles(
 
   return result;
 }
-
-/**
- * Get statistics about found files
- */
-export type FileStats = {
-  total: number;
-  byExtension: Record<string, number>;
-  byType?: Record<FileType, number>;
-};
-
-export function getFileStats(files: FileNode[]): FileStats {
-  const stats: FileStats = {
-    total: files.length,
-    byExtension: {},
-  };
-
-  for (const file of files) {
-    if (file.extension) {
-      stats.byExtension[file.extension] = (stats.byExtension[file.extension] || 0) + 1;
-    }
-  }
-
-  return stats;
-}
