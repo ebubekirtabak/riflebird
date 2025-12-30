d30ce415fdef7c693ce563bd0a3088e7ea3501f7
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2025-12-30 19:17:18 +0300

    feat(RF-14): add document in test type

--- packages/core/src/commands/fire/constants.ts
+++ packages/core/src/commands/fire/constants.ts
@@ -2,6 +2,14 @@ import type { TestType, TestScope } from '../fire-command';
 
 export const SUPPORTED_TEST_TYPES: TestType[] = ['unit'];
 
-export const ALL_TEST_TYPES: TestType[] = ['unit', 'e2e', 'visual', 'performance'];
+export const ALL_TEST_TYPES: TestType[] = ['unit', 'e2e', 'visual', 'performance', 'document'];
 
-export const SUPPORTED_TEST_SCOPES: TestScope[] = ['component', 'layout', 'page', 'service', 'util', 'hook', 'store'];
+export const SUPPORTED_TEST_SCOPES: TestScope[] = [
+  'component',
+  'layout',
+  'page',
+  'service',
+  'util',
+  'hook',
+  'store',
+];
