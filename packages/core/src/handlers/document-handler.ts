a36e9488fa1b9ad8128e67f5a83699c420f7ab50
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2026-01-03 15:00:41 +0300

    feat(RF-14): add document handler

---
+++ packages/core/src/handlers/document-handler.ts
@@ -0,0 +1,41 @@
+import { CommandContext } from '@commands/base';
+import type { FileNode, ProjectContext } from '@models';
+import { ProjectContextProvider } from '@providers/project-context-provider';
+import { StorybookRunner } from '@/runners/storybook-runner/storybook-runner';
+import { FireInput } from '@commands/fire/types';
+import { info } from '@utils';
+
+export class DocumentHandler {
+  constructor(private context: CommandContext) {}
+
+  async handle(
+    projectRoot: string,
+    provider: ProjectContextProvider,
+    projectContext: ProjectContext,
+    input: FireInput,
+    matchedFiles: FileNode[]
+  ): Promise<string[]> {
+    const documentationConfig = this.context.config.documentation;
+    if (!documentationConfig) {
+      return [];
+    }
+
+    const { framework, enabled } = documentationConfig;
+    if (!enabled) {
+      return [];
+    }
+
+    const framewors = {
+      storybook: () => {
+        const storybookRunner = new StorybookRunner(this.context);
+        return storybookRunner.run(projectRoot, provider, projectContext, input, matchedFiles);
+      },
+      none: () => {
+        info('Documentation framework not specified');
+        return [];
+      },
+    };
+
+    return framewors[framework as keyof typeof framewors]();
+  }
+}
